695bf152b8a2a3f7de7f73609627f81fed2bb128
지현 <spy03128@naver.com>
2021-08-16 17:28:21 +0900

    프로젝트 환경 설정

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://jihyeon:wlsdud369@cluster.hoxtt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log("err"));
+
+app.get("/", (req, res) => res.send("Hello Word!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
