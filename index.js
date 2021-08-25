1b51afc51616272dd155defa51b69397c55b4069
지현 <spy03128@naver.com>
2021-08-25 23:46:38 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://jihyeon:wlsdud369@cluster.hoxtt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log("err"));
 
-app.get("/", (req, res) => res.send("Hello Word!"));
+app.get("/", (req, res) => res.send("Hello Word! 아녕"));
+
+app.post("/register", (req, res) => {
+  //회원가입 시 필요한 정보를 가져와 DB에 넣음
+
+  const user = new User(req.body); //body 안에는 json 형식의 데이터가 들어있음
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
