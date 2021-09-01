d5c82a760926cfa5babc47d8968d46b8a67bf9ef
지현 <spy03128@naver.com>
2021-09-02 04:01:44 +0900

    권한별로 페이지 접근 다르게 지정하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./component/views/LandingPage/LandingPage";
 import LoginPage from "./component/views/LoginPage/LoginPage";
 import RegisterPage from "./component/views/RegisterPage/RegisterPage";
+import Auth from "./hoc/auth";
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/">
-            <LandingPage />
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-          <Route path="/register">
-            <RegisterPage />
-          </Route>
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+
+          <Route path="/login" component={Auth(LoginPage, false)} />
+
+          <Route path="/register" component={Auth(RegisterPage, false)} />
         </Switch>
       </div>
     </Router>
--- client/src/component/views/LoginPage/LoginPage.js
+++ client/src/component/views/LoginPage/LoginPage.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../../_action/user_action";
