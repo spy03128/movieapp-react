08e25dbbd479e542800d56edcc00e66d3b7826f9
지현 <spy03128@naver.com>
2021-09-02 02:55:28 +0900

    logout page

--- client/src/component/views/LandingPage/LandingPage.js
+++ client/src/component/views/LandingPage/LandingPage.js
@@ -1,10 +1,22 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { withRouter } from "react-router-dom";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃 하는데 실패했습니다");
+      }
+    });
+  };
+
   return (
     <div
       style={{
@@ -16,8 +28,10 @@ function LandingPage() {
       }}
     >
       <h2>시작페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
 
-export default LandingPage;
+export default withRouter(LandingPage);
