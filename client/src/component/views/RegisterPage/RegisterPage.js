39e7eb09ac83cc21d1b4e18cbc022dc7925a5072
지현 <spy03128@naver.com>
2021-09-02 02:49:10 +0900

    register page

--- client/src/component/views/RegisterPage/RegisterPage.js
+++ client/src/component/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_action/user_action";
+import { withRouter } from "react-router-dom";
 
-function RegisterPage() {
-  return <div>RegisterPage</div>;
+function RegisterPage(props) {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const onEmailHandler = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+  const onNameHandler = (e) => {
+    setName(e.currentTarget.value);
+  };
+
+  const onPasswordHandler = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const onConfirmPasswordHandler = (e) => {
+    setConfirmPassword(e.currentTarget.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault(); //새로고침 막아줌
+
+    if (password !== confirmPassword) {
+      return alert("비밀번호가 다릅니다");
+    }
+
+    let body = {
+      email: email,
+      name: name,
+      password: password,
+    };
+
+    dispatch(registerUser(body)).then((response) => {
+      if (response.payload.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to sign up");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={email} onChange={onEmailHandler} />
+        <label>Name</label>
+        <input type="text" value={name} onChange={onNameHandler} />
+        <label>Password</label>
+        <input type="password" value={password} onChange={onPasswordHandler} />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+
+        <br />
+        <button>회원가입</button>
+      </form>
+    </div>
+  );
 }
 
-export default RegisterPage;
+export default withRouter(RegisterPage);
