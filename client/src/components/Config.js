65860d112b2e2d16aea2e69170f4ace3d96da3b4
지현 <spy03128@naver.com>
2021-09-05 23:24:12 +0900

    main image 가지고오기

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "ebccc471cfe4c514afec7e9804c1db06";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
+import MainImage from "./Sections/MainImage";
 
 function LandingPage() {
-  return <></>;
+  const [movies, setMovies] = useState([]);
+  const [mainMovieImage, setMainMovieImage] = useState(null);
+
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((response) => {
+        setMovies([response.results]);
+        setMainMovieImage(response.results[0]);
+      });
+  }, []);
+
+  return (
+    <>
+      <div style={{ width: "100%", margin: "0" }}>
+        {mainMovieImage && (
+          <MainImage
+            image={`${IMAGE_BASE_URL}w1280${mainMovieImage.backdrop_path}`}
+            title={mainMovieImage.original_title}
+            text={mainMovieImage.overview}
+          />
+        )}
+        <div style={{ width: "85%", margin: "1rem auto" }}>
+          <h2>Movies by latest</h2>
+          <hr />
+
+          {/* {movie Grid Cards} */}
+        </div>
+
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button> Load More</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LandingPage;
